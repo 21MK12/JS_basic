1f52ea83f44137e9bf2fb3beca7ced0661392787
@BorisovM21 <mborisov@i.ua>
2022-06-08 21:40:32 +0300

    Работа с объектами, вложиними методами в объекты, также поигрался с JSON

--- Test/01_base.js
+++ Test/01_base.js
@@ -32,7 +32,7 @@
 //== не проверяет типы данных и приводит их к одной строке
 //=== сначала проверяет сначала типы данных
 
-// Функции
+//              Функции
 // function calculateAge(year) {
 //  return 2022 - year
 // }
@@ -51,7 +51,7 @@
 // logInfoAbout('Hellen', 1998);
 // logInfoAbout('Nick', 2030);
 
-// Массивы
+//              Массивы
 // const cars= ['Мазда', 'Мерседес', 'Форд'];
 // const cars = new Array('Мазда', 'Мерседес', 'Форд');
 // console.log(cars);
@@ -61,7 +61,7 @@
 // cars[cars.length] = 'Mazda' // добавление в конец масива
 // console.log(cars);
 
-//Циклы
+//              Циклы
 // const cars= ['Мазда', 'Мерседес', 'Форд'];
 
 // for (let i =0; i <cars.length; i++){
@@ -73,23 +73,76 @@
 //  console.log(car)
 // }
 
-// Объекты
+//           Объекты
 
-const person = {
- firstName: 'Michael',
- lastName: 'Borisov',
- year: 1993,
- languages: ['UA','EN','ru'],
- hasWife: false,
- greet: function () {
-  console.log('greet from person' )
- }
+// const person = {
+//  firstName: 'Michael',
+//  lastName: 'Borisov',
+//  year: 1993,
+//  languages: ['UA','EN','ru'],
+//  hasWife: false,
+//  greet: function () {
+//   console.log('greet from person' )
+//  }
+// }
+// console.log(person.firstName);
+// const key = 'year';
+// console.log(person[key]);
+// person.hasWife = true;
+// console.log(person)
+// person.isProgrammer = true;
+//
+// person.greet();
+
+// const myCity = {
+//     city: 'NewYork',
+//     isPopular: true,
+//                   //вложеный объект
+//     info: {
+//         descriptions: 'very interesting city!',
+//         isDifferent: 'true'
+//     }
+// }
+// console.log(myCity)
+//               точечная запись
+// delete myCity.isPopular
+//
+//  myCity['country'] = 'USA'
+//  console.log(myCity)
+//
+// const countryPropertyName = 'country'
+// myCity[countryPropertyName] = 'USA'
+// // скобочная запись
+// delete myCity.info["isDifferent"]
+// console.log(myCity.info.descriptions)
+
+// const name = 'Michael'
+// const postQty = 28
+// сокращенный формат записи
+// const userProfile ={
+//     name: name,
+//     postQty: postQty,
+//     isProgrammer: true
+// }
+// console.log(userProfile)
+//          Создание метода в теле объекта
+const myCity={
+    city: 'New York',
+    cityGreeting(){
+        console.log('Greetings!!!')
+    }
 }
-console.log(person.firstName);
-const key = 'year';
-console.log(person[key]);
-person.hasWife = true;
-console.log(person)
-person.isProgramer = true;
 
-person.greet();
+myCity.cityGreeting()
+
+// Работа с JSON
+const someMessage = {
+    name: 'Anna',
+    massage: 'Hello my pretty friend'
+}
+
+const parseToJSON = JSON.stringify(someMessage)
+console.log(parseToJSON)
+
+const parseToJS = JSON.parse(parseToJSON)
+console.log(parseToJS)
